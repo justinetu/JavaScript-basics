da38e90d6c7009c64d9a82d9c87b5629ec30d194
unknown <justinetu12@gmail.com>
2023-12-01 22:38:13 -0600

    update

---
+++ spreadOperator.js
@@ -0,0 +1,12 @@
+const states = ['Alabama', 'Kentucky', 'Houston'];
+
+const copy = [...states]
+//console.log(copy)
+
+const person = {
+    name: 'Peter Pan',
+    race: 'White'
+};
+
+const copyPerson = {...person};
+console.log(copyPerson)
